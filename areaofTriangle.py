a777c15d712795559b9da555bc88159f6302dfd6
SamradhniS <72724885+SamradhniS@users.noreply.github.com>
2021-10-05 11:35:51 +0530

    Create areaofTriangle.py

---
+++ areaofTriangle.py
@@ -0,0 +1,4 @@
+#Python program accepting base and of the height of Triangle from the user and compute the area.
+base= float(input("Enter the base length of triangle: "))
+height=float(input("Enter the height: "))
+print("Area of the Triangle with base "+str(base)+" and height "+str(height)+" is: "+ str(float((height*base)/2))+ "sq units")
